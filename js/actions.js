c9b64edb9c6e76ca05352b88218451c71ded65f3
Jakob Lykke <47578462+JakobJFL@users.noreply.github.com>
2021-06-01 21:12:03 +0200

    simple lights added

--- js/actions.js
+++ js/actions.js
@@ -1,10 +1,12 @@
 function changeRoomState(selectRoomId, setOn, setBri) {
-    let json = {
-        on: setOn,
-        bri: Number(setBri)
-    };
-    let url = 'http://'+acc.ip+'/api/'+acc.token+'/groups/'+selectRoomId+'/action/';
-    putRequest(url, json).then(response => {
-        getHueRooms(); // Refresh rooms html
-    }).catch(err => console.error(err));
+    return new Promise((resolve, reject) => {
+        let json = {
+            on: setOn,
+            bri: Number(setBri)
+        };
+        let url = 'http://'+acc.ip+'/api/'+acc.token+'/groups/'+selectRoomId+'/action/';
+        putRequest(url, json).then(response => {
+            resolve(response);
+        }).catch(err => reject(err));
+    })
 }
--- js/mainScript.js
+++ js/mainScript.js
@@ -1,10 +1,21 @@
+let acc;
+let selectedRoom = 0;
 document.getElementById("loginFrom").addEventListener("submit", setDashboard);
 
 function setDashboard() {
     getAccess();
-    getHueRooms();
+    setDashboardElements();
 }
-let acc;
+
+function setDashboardElements() {
+    getHueRooms().then(rooms => {   
+        getHueLights().then(lights => {
+            let dashboard = document.getElementById("dashboard");
+            dashboard.innerHTML = getDashboardHtml(rooms, lights);
+            document.getElementById("refreshBtn").addEventListener("click", setDashboardElements); 
+        });
+    });
+}
 
 function getAccess() {
     acc = {
@@ -14,24 +25,48 @@ function getAccess() {
 }
 
 function getHueRooms() {
-    getRequest('http://'+acc.ip+'/api/'+acc.token+'/groups').then(data => {
-        let rooms = [];
-        let i = 1;
-        while (data[i]) {
-            let roomObj = {
-                name: data[String(i)].name,
-                on: data[String(i)].state.any_on,
-                bri: data[String(i)].action.bri,
-                ct: data[String(i)].action.ct,
-                xy: data[String(i)].action.xy,
-                id: i
+    return new Promise((resolve, reject) => {
+        getRequest('http://'+acc.ip+'/api/'+acc.token+'/groups').then(data => {
+            console.log()
+            let rooms = [];
+            let fistIndex = Object.keys(data)[0];
+            selectedRoom = fistIndex;
+            let lastIndex = Object.keys(data).length;
+            for (let i = fistIndex; i <= lastIndex; i++) {
+                let roomObj = {
+                    name: data[String(i)].name,
+                    on: data[String(i)].state.any_on,
+                    bri: data[String(i)].action.bri,
+                    ct: data[String(i)].action.ct,
+                    xy: data[String(i)].action.xy,
+                    id: i
+                }
+                rooms.push(roomObj);
             }
-            rooms.push(roomObj);
-            i++;
-        }
-        let dashboard = document.getElementById("dashboard");
-        dashboard.innerHTML = getDashboardHtml(rooms);
-        document.getElementById("refreshBtn").addEventListener("click", getHueRooms);
+            resolve(rooms)
+        });
+    });
+}
+
+function getHueLights() {
+    return  new Promise((resolve, reject) => {
+        getRequest('http://'+acc.ip+'/api/'+acc.token+'/lights').then(data => {
+            let lights = [];
+            let i = 1;
+            while (data[i]) {
+                let roomObj = {
+                    name: data[String(i)].name,
+                    on: data[String(i)].state.on,
+                    bri: data[String(i)].state.bri,
+                    ct: data[String(i)].state.ct,
+                    xy: data[String(i)].state.xy,
+                    id: i
+                }
+                lights.push(roomObj);
+                i++;
+            }
+            resolve(lights)
+        });
     });
 }
 
